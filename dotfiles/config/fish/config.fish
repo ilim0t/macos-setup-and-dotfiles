048ebcf5057e3c9548fde199b908eec6030dd250
ilim <ilim0t14@gmail.com>
2025-07-29 23:56:26 +0900

    config.fishを更新

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -3,4 +3,18 @@
 # ~/.config/fish/config.fish
 # ──────────────────────────────────────────────
 
-set -gx PATH /opt/homebrew/bin $PATH
+if command -q brew
+    set -gx PATH (brew --prefix)/bin $PATH
+
+    # keg-only な Homebrew Formulaのバイナリを PATH に追加
+    set -l keg_only_packages curl unzip trash
+    set -l package_prefixes (brew --prefix $keg_only_packages)
+
+    for prefix in $package_prefixes
+        fish_add_path -g $prefix/bin
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
